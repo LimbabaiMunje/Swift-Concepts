e52b62fe911136da1fd22f00a050e24c19285911
LimbabaiMunje <limbabai.munje@definelabs.in>
2020-09-12 17:59:32 +0530

    New Commeit

---
+++ AutoLayoutPart2/AutoLayoutPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPart2
+//
+//  Created by Mac on 12/09/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
