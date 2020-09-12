b2c2932669cb473e5b40de8d9527b0d82f1aecc5
LimbabaiMunje <limbabai.munje@definelabs.in>
2020-09-12 08:28:10 +0530

    New Commit

---
+++ Advanced View/Advanced View/Model/PickerModel.swift
@@ -0,0 +1,14 @@
+//
+//  PickerModel.swift
+//  Advanced View
+//
+//  Created by Mac on 11/09/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import Foundation
+struct PickerModel {
+    static var sharedInstance = PickerModel()
+    var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satuday"]
+    var colors = ["Red","Pink","Blue","Green","Yellow","Black","White"]
+}
